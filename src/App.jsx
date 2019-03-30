4c41e6330bc3dc584eedb0049c873161eaf29e71
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-03-30 18:28:43 +0100

    Added logo and content container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer.jsx'
+import MainContent from './components/MainContent.jsx'
 
 export default class App extends React.Component {
   render() {
     return (
       <main>
-        <section>
-          ddf
-        </section>
+        <MainContent />
         <Footer />
       </main>
     );
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MainContent extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="logo-container">
+          <a className="logo">d'inks</a>
+        </div>
+        <div className="content-container">
+        </div>
+      </section>
+    );
+  }
+}
