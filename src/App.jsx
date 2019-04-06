a9cf0585341bd934b6d5db945dea84ccf6c66632
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-04-06 18:08:15 +0200

    Added clock to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Footer from './components/Footer.jsx'
-import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx';
+import MainContent from './components/MainContent.jsx';
+import Clock from './components/Clock.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
       <main className="main-container">
+        <Clock   />
         <MainContent />
         <Footer />
       </main>
