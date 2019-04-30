1846f8b36c6e50cdf6d34684fdcf6c57c7a96f1b
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-04-30 16:19:26 +0200

    First tests

--- src/tests/fib.test.js
+++ src/tests/fib.test.js
@@ -1,4 +1,12 @@
+const fib = require('../scripts/fib.js');
 
-test('test', () => {
-  expect(true).toBe(true);
+test('Negative length fibonacci list', () => {
+  function negativeFib() {
+    return fib(-5);
+  }
+  expect(negativeFib).toThrow('Num cannot be negative');
+});
+
+test('Empty fibonacci list', () => {
+  expect(fib(0)).toEqual([]);
 });
