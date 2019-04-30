8ed8893d0e836ef3d2783fb9cdf928cabb1682ad
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-04-30 16:24:22 +0200

    Additionall tests

--- src/tests/fib.test.js
+++ src/tests/fib.test.js
@@ -1,6 +1,6 @@
 const fib = require('../scripts/fib.js');
 
-test('Negative length fibonacci list', () => {
+test('Negative length fibonacci list - error', () => {
   function negativeFib() {
     return fib(-5);
   }
@@ -9,4 +9,14 @@ test('Negative length fibonacci list', () => {
 
 test('Empty fibonacci list', () => {
   expect(fib(0)).toEqual([]);
+});
+
+test('Simple short fibonacci list content correctness', () => {
+  expect(fib(10)).toEqual(
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  );
+})
+
+test('Long fibbonacci list length correctness', () => {
+  expect(fib(1000).length).toBe(1000);
 });
