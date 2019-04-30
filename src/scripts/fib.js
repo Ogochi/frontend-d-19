2824e81f466fa780f23dc332935f8f4cfb404bc6
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-04-30 16:19:18 +0200

    Fib implementation fix

--- src/scripts/fib.js
+++ src/scripts/fib.js
@@ -1,12 +1,20 @@
 
-export function fib(num) {
+function fib(num) {
   let result = [];
 
+  if (num < 0) {
+    throw 'Num cannot be negative';
+  }
+
   let first = 0, second = 1;
-  for (int i = 0; i < num; i++) {
+  for (let i = 0; i < num; i++) {
     result.push(first);
 
     second += first;
     first = second - first;
   }
+
+  return result;
 }
+
+module.exports = fib;
